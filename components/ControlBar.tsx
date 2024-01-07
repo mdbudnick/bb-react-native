e892315a498f86c2f6561a94018a561d21f05e58
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-06 20:51:26 -0500

    Remove css import

--- components/ControlBar.tsx
+++ components/ControlBar.tsx
@@ -1,5 +1,4 @@
 import React, { type FC } from 'react'
-import '../styles/control-bar.css'
 import '../img/play-pause.svg'
 import Timer from './Timer'
 import { type ConfigInput } from '../ts/shared'
