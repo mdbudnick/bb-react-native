b9176de464a9f66eb6b9c36648d1c3adb1dd732a
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-08 16:31:26 -0500

    Begin implementing Circle animation with react-native-reanimated

--- components/Circle.tsx
+++ components/Circle.tsx
@@ -1,8 +1,9 @@
-import React, { type FC, useEffect, useRef, useState } from 'react'
-import { Animated, Easing, View } from 'react-native'
+import React, { type FC, useEffect } from 'react'
+import { Easing } from 'react-native'
+import Animated, { useSharedValue, withTiming } from 'react-native-reanimated'
 
-const LARGE_CIRCLE_SIZE = 6
-const SMALL_CIRCLE_SIZE = 2
+const LARGE_CIRCLE_SIZE = 60
+const SMALL_CIRCLE_SIZE = 20
 
 const SMOOTH_PATH_TIMING = 1000
 const BREATH_CURVE = Easing.bezier(0.13, 0.38, 0.48, 1.02)
@@ -20,96 +21,73 @@ interface CircleProps {
 }
 
 const Circle: FC<CircleProps> = (props) => {
-  const circleSize = useRef(new Animated.Value(SMALL_CIRCLE_SIZE)).current
-  const circleBottom = useRef(new Animated.Value(props.boundingHeight)).current
-  const circleLeft = useRef(new Animated.Value(0)).current
+  let circleSize = useSharedValue(SMALL_CIRCLE_SIZE)
+  let circleBottom = useSharedValue(20)
+  let circleLeft = useSharedValue(20)
 
-  const [circleStyle, setCircleStyle] = useState({
-    width: SMALL_CIRCLE_SIZE,
-    height: SMALL_CIRCLE_SIZE,
-    backgroundColor: 'rgb(245, 121, 112)',
-    borderRadius: 50,
-    transformOrigin: 'center',
-    bottom: props.boundingHeight,
-    left: props.boundingWidth
-  })
+  const inhaleAnimation = () => {
+    console.log("inhaleAnimation")
+    circleLeft.value = withTiming(20)
+    circleBottom.value = withTiming(200)
+    circleSize.value = withTiming(LARGE_CIRCLE_SIZE)
+  }
 
-  const toBottomLeft = Animated.parallel([
-    Animated.timing(circleSize, {
-      toValue: SMALL_CIRCLE_SIZE,
-      duration: SMOOTH_PATH_TIMING,
-      easing: BREATH_CURVE,
-      useNativeDriver: false
-    }),
-    Animated.timing(circleBottom, {
-      toValue: props.boundingHeight,
-      duration: SMOOTH_PATH_TIMING,
-      easing: BREATH_CURVE,
-      useNativeDriver: false
-    }),
-    Animated.timing(circleLeft, {
-      toValue: -(SMALL_CIRCLE_SIZE / 2),
-      duration: SMOOTH_PATH_TIMING,
-      easing: BREATH_CURVE,
-      useNativeDriver: false
-    })
-  ])
-  // TODO - real toBottomRight
-  const toBottomRight = toBottomLeft
-  const toTopLeft = Animated.parallel([
-    Animated.timing(circleSize, {
-      toValue: LARGE_CIRCLE_SIZE,
-      duration: SMOOTH_PATH_TIMING,
-      easing: BREATH_CURVE,
-      useNativeDriver: false
-    }),
-    Animated.timing(circleBottom, {
-      toValue: 10,
-      duration: SMOOTH_PATH_TIMING,
-      useNativeDriver: false
-    }),
-    Animated.timing(circleLeft, {
-      toValue: -(SMALL_CIRCLE_SIZE / 2),
-      duration: SMOOTH_PATH_TIMING,
-      useNativeDriver: false
-    })
-  ])
-  // TODO - real toTopRight
-  const toTopRight = toTopLeft
+  const holdInhaleAnimation = () => {
+    console.log("holdInhaleAnimation")
+    circleLeft.value = withTiming(200)
+    circleBottom.value = withTiming(200)
+    circleSize.value = withTiming(LARGE_CIRCLE_SIZE)
+  }
+
+  const exhaleAnimation = () => {
+    console.log("exhaleAnimation")
+    circleLeft.value = withTiming(200)
+    circleBottom.value = withTiming(20)
+    circleSize.value = withTiming(SMALL_CIRCLE_SIZE)
+  }
+
+  const holdExhaleAnimation = () => {
+    console.log("holdExhaleAnimation")
+    circleLeft.value = withTiming(20)
+    circleBottom.value = withTiming(20)
+    circleSize.value = withTiming(SMALL_CIRCLE_SIZE)
+  }
 
   useEffect(() => {
-    toTopLeft.stop()
-    toBottomLeft.start()
+    console.log("reset")
+    holdExhaleAnimation()
   }, [props.reset])
 
   useEffect(() => {
-    toTopLeft.start()
+    console.log("inhale")
+    inhaleAnimation()
   }, [props.inhale])
 
   useEffect(() => {
-    toTopRight.start()
+    console.log("holdInhale")
+    holdInhaleAnimation()
   }, [props.holdInhale])
 
   useEffect(() => {
-    toBottomRight.start()
+    console.log("exhale")
+    exhaleAnimation()
   }, [props.exhale])
 
   useEffect(() => {
-    toBottomLeft.start()
+    console.log("holdExhale")
+    holdExhaleAnimation()
   }, [props.holdExhale])
 
-  useEffect(() => {
-    const diameter = parseInt(JSON.stringify(circleSize))
-    setCircleStyle({
-      ...circleStyle,
-      width: diameter,
-      height: diameter,
-      bottom: parseInt(JSON.stringify(circleBottom)),
-      left: parseInt(JSON.stringify(circleLeft))
-    })
-  }, [circleSize, circleBottom, circleLeft])
-
-  return <View style={[{ position: 'absolute' }, circleStyle]} />
+  return <Animated.View style={{
+    position: 'absolute',
+    width: circleSize,
+    height: circleSize,
+    backgroundColor: 'rgb(245, 221, 112)',
+    borderRadius: 50,
+    bottom: circleBottom,
+    left: circleLeft
+    }}
+  />
 }
 
 export default Circle
