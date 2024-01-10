f7b2613c69dbe6c48bba06262dc6eb96c683be1e
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-09 20:51:09 -0500

    Polish the bigGreenButton a bit

--- ts/stylesheet.ts
+++ ts/stylesheet.ts
@@ -14,14 +14,16 @@ const commonStyles = StyleSheet.create({
     display: 'none',
   },
   bigGreenButton: {
+    color: 'green',
     alignSelf: 'flex-start',
-    fontSize: 25,
+    fontSize: 30,
+    fontWeight:  800,
     borderWidth: 4,
     borderColor: 'green',
     backgroundColor: 'lightgreen',
     borderRadius: 25,
     marginRight: 20,
-    padding: 25,
+    padding: 20,
     paddingVertical: 15
   }
 })
