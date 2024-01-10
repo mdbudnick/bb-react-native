cdcae6205ba1a3588414fcb179dd99b7659f4159
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-09 20:51:27 -0500

    Remove the unused container style

--- ts/stylesheet.ts
+++ ts/stylesheet.ts
@@ -1,11 +1,6 @@
 import { StyleSheet } from 'react-native'
 
 const commonStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   linkText: {
     color: 'blue',
     textDecorationLine: 'underline',
